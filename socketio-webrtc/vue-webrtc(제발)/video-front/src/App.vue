0c3b01e9656c5143db634cc28af4bd03803fbad7
정용원 <asdfg0237@naver.com>
2020-09-18 13:54:26 +0900

    webrtc

---
+++ socketio-webrtc/vue-webrtc(제발)/video-front/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+
+  text-align: center;
+  color: #2c3e50;
+
+}
+</style>
